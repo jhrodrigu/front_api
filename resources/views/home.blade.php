b73021d265c729626c54cbb7137ffa0ffea7f2d3
Yonny Rodriguez <jhrodrigu@hotmail.com>
2022-09-23 20:16:18 -0500

    diseño

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,14 @@
     <h2>Fotografias</h2>
 
     @foreach ($fotosArray as $fotos)
-        <div class="card">
-            <img src="{{ $fotos['urls']['small'] }}" alt="Fotografia" for="seleccionar">
-            <input class="form-check-input" type="radio" id="seleccionar">
-        </div>
+
+        <div class="card" style="width: 18rem;">
+            <img src="{{ $fotos['urls']['small'] }}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Creada: {{ $fotos['created_at'] }}
+                <input class="form-check-input" type="radio" id="selec"></p>
+            </div>
+          </div>
 
     @endforeach
 @endsection
